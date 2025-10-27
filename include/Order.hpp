0138c19bc1dbd955c2d226ea9a5e7deb7eec257b
Youssef <youssef.ibrahimali2007@gmail.com>
2025-10-27 19:03:52 +0300

    Completed the Order base class

--- include/Order.hpp
+++ include/Order.hpp
@@ -1,19 +1,46 @@
 #pragma once
 
-#include <string>
+enum OrderProgress
+{
+    WAITING,
+    IN_SERVICE,
+    FINISHED,
+    CANCELLED
+};
+
+enum OrderType
+{
+    NORMAL,
+    VEGAN,
+    VIP
+};
 
 class Order
 {
 private:
+    int ID;
     int arrivalTime;
+    int assignedTime;
+    int finishTime;
+    OrderProgress progress;
     int size;
     double price;
 
 public:
-    Order(int arrivalTime, int size, double price);
+    Order(int ID, int arrivalTime, int size, double price);
     virtual ~Order() = default;
+    int getID() const;
     int getArrivalTime() const;
+    int getAssignedTime() const;
+    int getFinishTime() const;
+    int getWaitingTime() const;
+    int getServiceTime() const;
     int getSize() const;
     double getPrice() const;
-    virtual std::string getType() const = 0;
+    OrderProgress getProgress() const;
+    void setAssignedTime(int assignedTime);
+    void setFinishTime(int finishTime);
+    void setPrice(double price);
+    void setProgress(OrderProgress progress);
+    virtual OrderType getType() const = 0;
 };
