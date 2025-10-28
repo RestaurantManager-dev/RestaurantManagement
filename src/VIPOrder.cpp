4b4b8796722cf7ca45684f1448ee051d7beae9dd
Youssef <youssef.ibrahimali2007@gmail.com>
2025-10-28 20:35:03 +0300

    Added NormalOrder, VeganOrder, VIPOrder classes

---
+++ src/VIPOrder.cpp
@@ -0,0 +1,10 @@
+#include "VIPOrder.hpp"
+
+VIPOrder::VIPOrder(int ID, int arrivalTime, int size, double price) : Order(ID, arrivalTime, size, price)
+{
+}
+
+OrderType VIPOrder::getType() const
+{
+    return VIP;
+}
