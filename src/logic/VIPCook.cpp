c8f2f047e397dbae4f534f467f54f070a73bc2de
Youssef <youssef.ibrahimali2007@gmail.com>
2025-10-30 20:37:11 +0300

    Added VIPCook sub-class

---
+++ src/logic/VIPCook.cpp
@@ -0,0 +1,10 @@
+#include "VIPCook.hpp"
+
+VIPCook::VIPCook(int ID, int speed, int breakDuration, int ordersBeforeBreak) : Cook(ID, speed, breakDuration, ordersBeforeBreak)
+{
+}
+
+CookType VIPCook::getType() const
+{
+    return CookType::VIP;
+}
