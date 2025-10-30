f3f19a11587a2616835806e06598d013ef5117e2
Youssef <youssef.ibrahimali2007@gmail.com>
2025-10-30 21:12:35 +0300

    refactor(NormalOrder): corrected style

--- include/NormalOrder.hpp
+++ include/NormalOrder.hpp
@@ -2,12 +2,10 @@
 
 #include "Order.hpp"
 
-class NormalOrder: public Order
+class NormalOrder : public Order
 {
 public:
     NormalOrder(int ID, int arrivalTime, int size, double price);
     ~NormalOrder() = default;
     virtual OrderType getType() const override;
 };
-
-
